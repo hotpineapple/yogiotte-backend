ef4a77c8ae6aad521019700bc7dbe2efd9988a9b
jh choi <hence1756@gmail.com>
2022-03-05 17:48:13 +0900

    로그아웃

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/join', isNotLoggedIn, async (req, res, next) => {
 
     const { username, password } = req.body;
-   
+
     try {
         const exUser = await User.findOne({ where: { name: username } });
         if (exUser) {
@@ -21,7 +21,7 @@ router.post('/join', isNotLoggedIn, async (req, res, next) => {
             password: hash,
         });
 
-        return res.send('register success');
+        return res.send('true');
     } catch (error) {
 
         console.log(error);
@@ -36,17 +36,17 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
         }
         if (!user) {
             console.log(info.message);
-            return res.status(500).send(new Error('no user'));
+            return res.status(500).json({ error: 'no user' });
         }
         return req.login(user, (loginError) => {
 
             if (loginError) {
                 console.log(loginError);
-                return res.status(500).send(new Error('password not match'));
+                return res.status(500).json({ error: 'password not match' });
                 // return next(loginError)
             }
             // console.log(user.dataValues.name);
-            return res.status(200).send({ auth: 'true', user: user.dataValues.name });
+            return res.status(200).json({ auth: 'login', userId: user.dataValues.name });
             // return res.redirect('/');
         });
     })(req, res, next);
@@ -55,7 +55,9 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
 router.get('/logout', isLoggedIn, (req, res)=> {
     req.logout(); 
     req.session.destroy();
-    res.redirect('/');
+    // res.redirect('/');
+    res.json({auth: 'logout'});
+    console.log('로그아웃처리완료');
 });
 
 module.exports = router;
