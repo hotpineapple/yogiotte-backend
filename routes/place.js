6ca688e851d3c267cf252511597f152130331425
jh choi <hence1756@gmail.com>
2022-03-07 16:12:58 +0900

    페이징 구현

--- routes/place.js
+++ routes/place.js
@@ -10,7 +10,7 @@ const Op = sequelize.Op;
 router.get('/one/:id', (req, res) => {
     Place.findOne({ where: { id: req.params.id, img: { [Op.like]: 'http%' }}})
         .then((place) => {
-             res.status(200).send(place);
+            res.status(200).send(place);
         })
         .catch((error) => {
             return res.status(500).json({
@@ -21,11 +21,14 @@ router.get('/one/:id', (req, res) => {
 });
 
 router.get('/hot', (req, res) => {
-    Place.findAll({ offset: 0, limit: 10, where: {img: { [Op.like]: 'http%' }}  })
+    const limit = req.query.limit;
+    Place.findAll({ where: {img: { [Op.like]: 'http%' }}, offset: limit-10, limit: limit-0 })
         .then((places) => {
-             res.status(200).send(places);
+            if (limit == 10) res.status(200).send({ more: false, places });
+            else res.status(200).send({ more: true, places });
         })
         .catch((error) => {
+            console.log(error);
             return res.status(500).json({
                 code: 500,
                 message: '서버에러'
@@ -34,9 +37,11 @@ router.get('/hot', (req, res) => {
 });
 
 router.get('/bar', (req, res) => {
-    Place.findAll({ where: { maintype: '주점', img: { [Op.like]: 'http%' } }, offset: 0, limit: 10  })
+    const limit = req.query.limit;
+    Place.findAll({ where: { maintype: '주점', img: { [Op.like]: 'http%' } }, offset: limit-10, limit: limit-0 })
         .then((places) => {
-             res.status(200).send(places);
+            if (limit == 10) res.status(200).send({ more: false, places });
+            else res.status(200).send({ more: true, places });
         })
         .catch((error) => {
             return res.status(500).json({
@@ -47,9 +52,11 @@ router.get('/bar', (req, res) => {
 });
 
 router.get('/cafe', (req, res) => {
-    Place.findAll({ where: { maintype: '카페', img: { [Op.like]: 'http%' }}, offset: 0, limit: 10 })
+    const limit = req.query.limit;
+    Place.findAll({ where: { maintype: '카페', img: { [Op.like]: 'http%' }}, offset: limit-10, limit: limit-0 })
         .then((places) => {
-            res.status(200).send(places);
+            if (limit == 10) res.status(200).send({ more: false, places });
+            else res.status(200).send({ more: true, places });
         })
         .catch((error) => {
             return res.status(500).json({
@@ -60,9 +67,11 @@ router.get('/cafe', (req, res) => {
 });
 
 router.get('/restaurant', (req, res) => {
-    Place.findAll({ where: { maintype: '음식점',img: { [Op.like]: 'http%' } }, offset: 0, limit: 10  })
+    const limit = req.query.limit;
+    Place.findAll({ where: { maintype: '음식점',img: { [Op.like]: 'http%' } }, offset: limit-10, limit: limit-0 })
         .then((places) => {
-             res.status(200).send(places);
+            if (limit == 10) res.status(200).send({ more: false, places });
+            else res.status(200).send({ more: true, places });
         })
         .catch((error) => {
             return res.status(500).json({
@@ -73,10 +82,14 @@ router.get('/restaurant', (req, res) => {
 });
 
 router.get('/search/:keyword', (req, res) => {
-    
-    Place.findAll({ where: { name: { [Op.like]: '%' + req.params.keyword + '%' } }, offset: 0, limit: 10  })
+    const limit = req.query.limit;
+    Place.findAll({ where: { name: { [Op.like]: '%' + req.params.keyword + '%' } }, offset: limit-10, limit: limit-0 })
         .then((places) => {
-             res.status(200).send(places);
+            if (limit == 10) {
+                console.log('첫페이지 조회');
+                res.status(200).send({ more: false, places });
+            }
+            else res.status(200).send({ more: true, places });
         })
         .catch((error) => {
             return res.status(500).json({
@@ -87,10 +100,10 @@ router.get('/search/:keyword', (req, res) => {
 });
 
 router.get('/location/:code', (req, res) => {
-    console.log('..?');
-    Place.findAll({ where: { code: req.params.code }, offset: 0, limit: 10  })
+    // console.log('..?');
+    Place.findAll({ where: { code: req.params.code }, /*offset: 0, limit: limit-0*/  })
         .then((places) => {
-             res.status(200).send(places);
+            res.status(200).send(places);
         })
         .catch((error) => {
             return res.status(500).json({
